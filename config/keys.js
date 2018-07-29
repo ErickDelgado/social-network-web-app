15b7f62b1d34da3cf649ea4e7c674045a2514d9c
Erick Delgado <erickdelgado@protonmail.com>
2018-07-29 12:45:48 -0400

    setting up routes
    Co-Authored-By: Erick Delgado <erickdelgado@users.noreply.github.com>

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+  'mongodb://erick:swoles123@ds141671.mlab.com:41671/socialnetworkdb'
+}
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
-app.get('/' (req, res) => res.send('Hello'));
-const port = 5000;
+const mongoose = require('mongoose');
+const db = require('./config/keys.js').mongoURI;
+mongoose
+  .connect(db)
+  .then((err) => console.log('Mongo connected!'))
+  .catch((err) => console.log(err));
+app.get('/', (req, res) => res.send('Hello'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));

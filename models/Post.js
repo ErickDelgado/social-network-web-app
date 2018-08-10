098731a6c24b0eeeca43be52f7c25aa584b29575
Erick Delgado <erickdelgado@protonmail.com>
2018-08-10 00:36:46 -0300

    validations for post, debug post model
    Co-Authored-By: Erick Delgado <erickdelgado@users.noreply.github.com>

--- models/Post.js
+++ models/Post.js
@@ -28,10 +28,6 @@ const PostSchema = new Schema({
       type: Schema.Types.ObjectId,
       ref: 'users'
     },
-    text: {
-      type: String,
-      required: true
-    },
     avatar: {
       type: String
     }
--- validations/post.js
+++ validations/post.js
@@ -1,19 +1,19 @@
 const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
-module.exports = function validateLoginInput(data) {
+module.exports = function validatePostInput(data) {
   let errors = {};
 
   // Ensuring we are testing a string
   // Validator can only be used in conjunction with strings
   data.text = !isEmpty(data.email) ? data.email : '';
 
-  if(Validator.isEmpty(data.text)) {
-    errors.text = 'text field is required';
+  if(!Validator.isLength(data.text, {min:1, max:300})) {
+    errors.text = 'Post must be between 1 and 300 characters';
   }
 
-  if(!Validator.isEmail(data.email)) {
-    errors.email = 'Email field is invalid';
+  if(Validator.isEmpty(data.text)) {
+    errors.text = 'text field is required';
   }
 
   return {

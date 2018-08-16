337e679e43c64a1f639d079d01cc4aa93dd047d0
Erick Delgado <erickdelgado@protonmail.com>
2018-08-15 22:59:11 -0300

    fixed validation bugs
    Co-Authored-By: Erick Delgado <erickdelgado@users.noreply.github.com>

--- validations/register.js
+++ validations/register.js
@@ -32,11 +32,11 @@ module.exports = function validateRegisterInput(data) {
     errors.password = 'Password field is required';
   }
 
-  if(Validator.isLength(data.password, {min:6, max:30})) {
+  if(!Validator.isLength(data.password, {min:6, max:30})) {
     errors.password = "Password must be at least 6 characters";
   }
 
-  if(Validator.equals(data.password, data.password2)) {
+  if(!Validator.equals(data.password, data.password2)) {
     errors.password2 = 'Passwords must match';
   }
 

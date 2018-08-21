f8987dabca45c59ee575d5705d7f461fe82a2f7c
Erick Delgado <erickdelgado@protonmail.com>
2018-08-21 19:05:39 -0300

    redux boilerplate
    Co-Authored-By: Erick Delgado <erickdelgado@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
-
+import store from './store';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -10,20 +10,21 @@ import Login from './components/auth/login';
 
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
       <Provider>
         <Router>
-        <div className="App">
-        <Navbar/>
-        <Route exact path ="/" component = {Landing} />
-        <div className="container">
-        <Route exact path="/register" component={Register}/>
-        <Route exact path="/login" component={Login}/>
-        </div>
-        <Footer/>
-        </div>
+          <div className="App">
+            <Navbar/>
+            <Route exact path ="/" component = {Landing} />
+            <div className="container">
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/login" component={Login}/>
+            </div>
+            <Footer/>
+          </div>
         </Router>
       </Provider>
     );

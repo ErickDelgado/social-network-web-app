76e4edf580fa570e71ec9c0f4153f7ef2ff2e465
Erick Delgado <erickdelgado@protonmail.com>
2018-08-25 23:30:37 -0300

    registration reducers/actions
    Co-Authored-By: Erick Delgado <erickdelgado@users.noreply.github.com>

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,42 @@
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+// Set Logged in users
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
 
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post('/api/users/register', userData)
     .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+}
+
+export const loginUser = userData => dispatch => {
+  axios.post('api/users/login', userData)
+    .then(res => {
+      // Save to local storage
+      const { token } = res.data;
+      // Set token to local storage
+      localStorage.setItem('jwtToken', token);
+      // Set token to auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set the current users
+      dispatch(setCurrentUser(decoded));
+    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
